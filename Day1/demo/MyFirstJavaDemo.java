5fed201085daa522fcf9a8c415d898f444565720
kbsbhanu <kbhanu0809@gmail.com>
2022-01-30 20:11:44 +0530

    my day1 java prgs

---
+++ Day1/demo/MyFirstJavaDemo.java
@@ -0,0 +1,26 @@
+package com.demo;
+
+public class MyFirstJavaDemo {
+	
+	public static void main(String args[]) {
+		
+		int a=10;
+		int b=30;
+		
+		int temp=a+b;// logic for add of two
+		
+		int temp1=a-b;
+		
+		int  length=25;
+		int breadth=25;
+		
+		int areaofreactage=length*breadth;
+		
+		System.out.println("sum of two numbers   --->"+temp);
+		
+		System.out.println("sub of two numbers   --->"+temp1);
+		
+		System.out.println("Area  of two numbers   --->"+areaofreactage);
+	}
+
+}
